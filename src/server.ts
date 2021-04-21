c597c3c082e3d3c0a5f0f7bcaacb21f39c64f035
RenatoFrancisco <renatofrancisco.s@gmail.com>
2021-04-21 10:31:09 -0300

    Aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
 import express from 'express';
 import './database';
+import { routes } from './routes';
 
 const app = express();
-app.get('/', (request, response) => {
-  return response.json({message: 'Hello, NLW 05!'});
-});
 
-app.post('/', (request, response) => {
-  return response.json({message: 'User has been saved'})
-});
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3333, () => console.log('Server is running on port 3333'));
